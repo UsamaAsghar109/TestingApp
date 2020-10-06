9c4ed9dd5846a4befab64eeb81c533511dfc69ee
SamSaim1122 <usamaasghar109@gmail.com>
2020-10-06 15:23:45 +0500

    Registration is done and the data is stored in the firebase

--- app/src/main/java/com/app/testingapp/LoginActivity.java
+++ app/src/main/java/com/app/testingapp/LoginActivity.java
@@ -1,11 +1,16 @@
 package com.app.testingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.ColorStateListDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -14,12 +19,24 @@ import android.widget.ToggleButton;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.spark.submitbutton.SubmitButton;
+
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText username, password;
     SubmitButton submitButton;
+    ProgressDialog progressDialog;
+
+    //Declare an instance of FirebaseAuth
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,27 +49,69 @@ public class LoginActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+        //Progress Dialog
+        progressDialog=new ProgressDialog(this);
+        progressDialog.setMessage("Registering User  .......");
 
         //Layout Button Setting
         setContentView(R.layout.activity_login);
         username=findViewById(R.id.edit_username);
         password=findViewById(R.id.edit_password);
         submitButton=findViewById(R.id.submit);
+
+        //Firebase Authentication
+        mAuth = FirebaseAuth.getInstance();
+
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(username==null)
+
+                String user=username.getText().toString().trim();
+                String pass=password.getText().toString().trim();
+
+                if(!Patterns.EMAIL_ADDRESS.matcher(user).matches())
                 {
-                    Toast.makeText(LoginActivity.this, "Username is empty", Toast.LENGTH_SHORT).show();
+                    username.setError("Invalid Email");
+                    username.setFocusable(true);
                 }
-                else if (password==null)
+                else if (password.length()<8)
                 {
-                    Toast.makeText(LoginActivity.this, "Password is empty", Toast.LENGTH_SHORT).show();
+                    password.setError("Invalid Password");
                 }
                 else{
-                    Toast.makeText(LoginActivity.this, "Successfull", Toast.LENGTH_SHORT).show();
+                    registerUser(user,pass);
+
                 }
 
+            }
+        });
+    }
+
+    private void registerUser(String user, String pass) {
+        progressDialog.show();
+        mAuth.createUserWithEmailAndPassword(user, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            progressDialog.dismiss();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(LoginActivity.this, "Registered Successful."+user.getEmail(),Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this,ProfileActivity.class));
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            progressDialog.dismiss();
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(LoginActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
